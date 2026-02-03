da6ec866dd8ff967711500cce0d2bf370e229cfa
lewisannan <lewisannan23@gmail.com>
2026-02-03 13:21:41 +0300

    initial commit

---
+++ introduction.py
@@ -0,0 +1,6 @@
+#print is a method used for displaying an output
+
+print("Welcome to Python Programming")
+print("Lewis")
+print(200)
+print(40*3)
---
+++ variables.py
@@ -0,0 +1,15 @@
+#variable is basically a container used to store a value
+
+school = "eMobilis"
+firstname = "Matthew"
+lastname = "Philip"
+num1 = 45
+num2 = 100
+language = "Python"
+
+print(firstname,lastname)
+print(school ,language,num1)
+print(school, "is a coding school")
+print(language,"is a web programming language")
+print("The sum of",num1,"and",num2,"is",num1 + num2 )
+
